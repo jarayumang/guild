960b0542ffe6dcc21625888f5cdd1cc61bb82df0
Janssen Radh Yumang <janssenreal23@gmail.com>
2024-12-02 15:02:29 +0800

    fixed header with correct components

--- app/(jobs)/layout.tsx
+++ app/(jobs)/layout.tsx
@@ -1,38 +1,112 @@
 'use client'
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation'
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Input } from "@/components/ui/input"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { MapPin } from "lucide-react";
 
 export default function JobsLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
-  const router = useRouter()
-
   return (
     <section>
-      <div className="flex row justify-between items-center px-10 py-5 bg-[#272727] text-[#EFF1F3]">
+      <div className="flex row justify-between items-center px-10 py-5 border-b-2">
         <div className="flex row items-center gap-10">
           <div className=''>
-            <Image 
-              src="/logo_dark.png"
+            <Image
+              src="/logo_light.png"
               alt="Company Logo"
               width={100}
               height={100}
             />
           </div>
-          <div className="flex row gap-7">
-            <div className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-2' onClick={() => router.push('/listing')}>Listings</div>
-            <div className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-2'>Messages</div>
-            <div className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-2' onClick={() => router.push('/company')}>Companies</div>
-            <div className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-2' onClick={() => router.push('/salary')}>Salary</div>
-            <div className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-2' onClick={() => router.push('/recommendation')}>FAQs</div>
-          </div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/listing" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Listings
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/listing" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Messages
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/company" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Companies
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/salary" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Salary
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/recommendation" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    FAQs
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
-        <div>
-          <div>Location</div>
-          <div>Profiles</div>
+        <div className='flex row items-center gap-10'>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <MapPin className="h-5 w-5 text-gray-500" />
+            </div>
+            <Input
+              placeholder="Location"
+              className="pl-10"
+            />
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </div>
       <nav></nav>
